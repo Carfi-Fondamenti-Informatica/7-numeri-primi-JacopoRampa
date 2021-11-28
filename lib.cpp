94fbadd4874a3a2e7c21cfc93b9e7f38114b6d0c
JacopoRampa <91597718+JacopoRampa@users.noreply.github.com>
2021-11-28 19:18:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
+bool numero_primo(int n, int d){
+    if(d==0 || d==1) {
+        return true;
+    }
+    else {
+        int c=n%d;
+        if(c!=0){
+            numero_primo(n, d-1);
+        }
+        else{
+            return false;
+        }
 
+    }
+}
